8afa0bf9cf7e928471d1a76c793e0ed5145e6431
mike <mike@emotive.com>
2012-12-10 10:57:28 -0800

    Add functions to check for reserved names

--- test/testMisc.js
+++ test/testMisc.js
@@ -7,3 +7,38 @@ var assert = require('assert');
 var buf = emutils.mergeIntoBuffer("abc", new Buffer("def"), "ghi");
 assert.equal(buf.toString(), "abcdefghi");
 assert.ok(Object.getPrototypeOf(buf).writeInt8);  // buf is a Buffer
+
+// test the reserved name functions
+assert.ok(emutils.isReservedTypeName("CDMType"));
+assert.ok(emutils.isReservedTypeName("CdMType"));
+assert.ok(emutils.isReservedTypeName("CdmType"));
+assert.ok(emutils.isReservedTypeName("CDMType", true));
+assert.ok(emutils.isReservedTypeName("CdMType", true));
+assert.ok(emutils.isReservedTypeName("CdmType", true));
+assert.ok(emutils.isReservedTypeName("_CDMType"));
+assert.ok(emutils.isReservedTypeName("__CdMType"));
+assert.ok(emutils.isReservedTypeName("___CdmType"));
+assert.ok(!emutils.isReservedTypeName("_CDMType", true));
+assert.ok(!emutils.isReservedTypeName("__CdMType", true));
+assert.ok(!emutils.isReservedTypeName("___CdmType", true));
+
+assert.equal(emutils.getCdmTypeName("foo"), "foo");
+assert.equal(emutils.getCdmTypeName("CdmType"), "_CdmType");
+assert.equal(emutils.getCdmTypeName("_CdmType"), "__CdmType");
+
+assert.ok(emutils.isReservedPropertyName("Id"));
+assert.ok(emutils.isReservedPropertyName("VerSioN"));
+assert.ok(emutils.isReservedPropertyName("TargetType"));
+assert.ok(emutils.isReservedPropertyName("Id", true));
+assert.ok(emutils.isReservedPropertyName("VerSioN", true));
+assert.ok(emutils.isReservedPropertyName("TargetType", true));
+assert.ok(emutils.isReservedPropertyName("Id$"));
+assert.ok(emutils.isReservedPropertyName("VerSioN$$"));
+assert.ok(emutils.isReservedPropertyName("TargetType$$$"));
+assert.ok(!emutils.isReservedPropertyName("Id$", true));
+assert.ok(!emutils.isReservedPropertyName("VerSioN$$", true));
+assert.ok(!emutils.isReservedPropertyName("TargetType$$$", true));
+
+assert.equal(emutils.getCdmPropertyName("foo"), "foo");
+assert.equal(emutils.getCdmPropertyName("targetType"), "targetType$");
+assert.equal(emutils.getCdmPropertyName("cdmqueryname"), "cdmqueryname$");
